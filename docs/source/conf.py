eac07f3a3aaac072b997829ed45ea2c23cae510a
z03h <earkz0@gmail.com>
2021-05-08 02:17:21 -0700

    sadge

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,8 @@ author = 'z03h'
 # -- General configuration ---------------------------------------------------
 needs_sphinx = '3.4.3'
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
